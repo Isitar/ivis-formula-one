724741f13ace42a99cf1f65c361c422255e604ac
Pascal Lüscher <pascal.luescher@asinfotrack.ch>
2019-04-30 15:20:59 +0200

    first request to fill drivers from api2

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -6,4 +6,21 @@
     document.querySelectorAll('.bar-racer').forEach(el => {
         el.classList.add('racing');
     });
+
+
+    fetch('http://ergast.com/api/f1/drivers.json')
+    .then(res => res.json())
+    .then(res => {
+        const selD1 = document.querySelector('#driver_1_select');
+        const selD2 = document.querySelector('#driver_2_select');
+        
+        res.MRData.DriverTable.Drivers.forEach(d => {
+            const opt = document.createElement('option');
+            opt.value=d.driverId;
+            opt.text = `${d.givenName} ${d.familyName}`;
+            selD1.appendChild(opt)
+            const opt2 = opt.cloneNode(true);
+            selD2.appendChild(opt2);
+        })
+    })
 })();
